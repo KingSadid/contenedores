364add12763ae5273e3b02ce19232e60d9be0c5d
406 <406@gmail.com>
2026-02-11 10:49:57 -0500

    Modify main

--- lib/main.dart
+++ lib/main.dart
@@ -13,39 +13,40 @@ class MiAplicacion extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Ejemplo Columna y Fila'),
+          title: const Text('Imagen, Texto e Iconos'),
           backgroundColor: Colors.amber,
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Moviles',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              Image.network(
+                'https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c',
+                height: 400,
+                width: 400,
+                fit: BoxFit.cover,
               ),
-              
-              const SizedBox(height: 20), 
+
+              const SizedBox(height: 20),
+
+              const Text(
+                'Móviles',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueGrey,
+                ),
+              ),
+
+              const SizedBox(height: 20),
 
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  
                   Icon(Icons.android, size: 50, color: Colors.green[700]),
-                  
-                  const SizedBox(width: 20), 
-
+                  const SizedBox(width: 30),
                   Icon(Icons.phone_iphone, size: 50, color: Colors.grey[800]),
                 ],
-              ),
-              
-              const SizedBox(height: 20), 
-
-              Container(
-                margin: const EdgeInsets.all(10.0),
-                color: Colors.amber[600],
-                width: 48.0,
-                height: 48.0,
               ),
             ],
           ),
